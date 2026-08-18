52bbb4b1c2aea0132ff407790fa424fadd835d85
chanams123 <chanamspielman@gmail.com>
2026-08-18 17:52:30 -0400

    Your message

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-
+import Highlight from './Highlight';
+import {faBolt, faBookOpen, faTags} from '@fortawesome/free-solid-svg-icons'
 
 const Highlights = () => {
     return (
@@ -12,47 +13,21 @@ const Highlights = () => {
                             </h2>
                         <div className="highlight__wrapper">
                             <Highlight 
-                            icon={<FontAwesomeIcon icon="bolt"/>}
+                            icon={<FontAwesomeIcon icon={faBolt}/>}
                             title="Easy and Quick"
                             para="Get access to the book you purchased instantly"
                         />
                         <Highlight 
-                            icon={<FontAwesomeIcon icon="book-open"/>}
+                            icon={<FontAwesomeIcon icon={faBookOpen}/>}
                             title="10,000+ Books"
                             para="Library has all your favorite categories."
                         />
                         <Highlight 
-                            icon={<FontAwesomeIcon icon="tags"/>}
+                            icon={<FontAwesomeIcon icon={faTags}/>}
                             title="Affordable Pricing"
                             para="Get your hands on popular books for as little as $10.00. We have a variety of books to choose from."
                         />
-                            <div className="highlight">
-                                <div className="highlight__img">
-                                    <font-awesome-icon icon="bolt" />
-                                </div>
-                                <h3 className="highlight__subtitle">Easy and Quick</h3>
-                                <p className="highlight__para">
-                                    Get access to the book you purchased instantlyl
-                                
-                                </p>
-                            </div>
-                            <div className="highlight">
-                                <div className="highlight__img">
-                                    <font-awesome-icon icon="book-open" />
-                                </div>
-                                <h3 className="highlight__subtitle">10,000+ Books</h3>
-                                <p className="highlight__para">
-                                    Library has all your favorite categories.
-                                </p>
-                            </div>
-                            <div className="highlight">
-                                <div className="highlight__img">
-                                    <font-awesome-icon icon="tags" />
-                                </div>
-                                <h3 className="highlight__subtitle">Affordable Pricing</h3>
-                                <p className="highlight__para">
-                                    Get your hands on popular books for as low as $10.00. We have a variety of books to choose from.
-                                </p>
+                        
                             </div>
                         </div>
                     </div>
