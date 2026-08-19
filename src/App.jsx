4213ee7b541502621bec374364806b5c821da52b
chanams123 <chanamspielman@gmail.com>
2026-08-19 16:40:26 -0400

    Your message

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Nav from './components/Nav';
 import Landing from './components/Landing';
 import Highlights from './components/highlights';
+import Featured from './components/Featured';
 
 function App() {
   return (
