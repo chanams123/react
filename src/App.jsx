ba3daa0c685692cb4692b76153715644b0a001a2
chanams123 <chanamspielman@gmail.com>
2026-08-27 14:29:01 -0400

    Your message

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import Footer from './components/Footer';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Books from './Pages/Books';
+import { books } from './data';
 
 function App() {
   return (
     <Router>
       <div className="App">
       <Nav />
-        <Route path="/books" exact component={Home} />
-        <Route path="/books" exact component={Books} />
-      <Home />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" render={() => <Books books={books} />} />
       <Footer />
       </div>
     </Router>
--- src/Pages/books.jsx
+++ src/Pages/books.jsx
@@ -1,34 +1,71 @@
-import React from 'react'
+import React from "react";
+import { books } from "../data";
+import Book from "../components/Book";
 
-const books = () => {
+const Books = ({books: initialBooks}) => {
+    const {books, setBooks} = useState(initialBooks);
+    
+    function filterBooks(filter) {
+        console.log(filter);
+        if (filter === "low_to_high") {
+            setBooks(
+                books
+                .slice()
+                .sort(
+                    (a,b) =>
+                         (a.salePrice || a.originalPrice) - 
+                         (b.salePrice || b.originalPrice)
+                        )
+                    );
+        }  
+        if (filter === "high_to_low") {
+            setBooks(
+                books
+                .slice()
+                .sort(
+                    (a,b) =>
+                         (b.salePrice || b.originalPrice) - 
+                         (a.salePrice || a.originalPrice)
+                        )
+                    );
+        }  
+        if (filter === "rating") {
+            setBooks(books.slice().sort((a, b) => b.rating - a.rating));
+        }
+    }
+
   return (
     <div id="books__body">
-    <main id="books__main">
-      <section>
-        <div className="books__container">
+      <main id="books__main">
+        <section>
+          <div className="books__container">
             <div className="row">
-                <div className="books__header">
-                    <h2 className="section__title books__header-title">All Books</h2>
-                    <select id="filter" defaultValue="DEFAULT">
-                        <option value="DEFAULT" disabled>Sort</option>
-                        <option value="low_to_high">Price, Low to High</option>
-                        <option value="high_to_low">Price, High to Low</option>
-                        <option value="rating">Rating</option>
-                    </select>
-                </div>
-                <div className="books">
-                    {
-                        books.map((book) => (
-                            <Book book={book} key={book.id} />
-                    }
-                    </div>
-                 </div>
-                </div>
-            </section>
-        </main>
-    </div>    
-      
-  );
-}
+              <div className="books__header">
+                <h2 className="section__title books__header-title">
+                  All Books
+                </h2>
 
-export default books;
+                <select id="filter" defaultValue="DEFAULT" onChange={(event) => filterBooks(event.target.value)}>
+                  <option value="DEFAULT" disabled>
+                    Sort
+                  </option>
+                  <option value="low_to_high">Price, Low to High</option>
+                  <option value="high_to_low">Price, High to Low</option>
+                  <option value="rating">Rating</option>
+                </select>
+              </div>
+
+              <div className="books">
+                {books.map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Books;
